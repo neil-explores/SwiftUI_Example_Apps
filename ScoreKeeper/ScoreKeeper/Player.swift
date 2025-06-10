7509c456b1ee2a512a808dea292717e24b431efb
Neil Thorne <neilsthorne@gmail.com>
2025-06-10 11:19:17 -0400

    add ScoreKeeper

---
+++ ScoreKeeper/ScoreKeeper/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  ScoreKeeper
+//
+//  Created by Neil Thorne on 6/10/25.
+//
+
+import Foundation
+
+struct Player: Identifiable {
+    var id = UUID()
+    
+    var name: String
+    var score: Int
+    
+    
+}
---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Neil Thorne on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
